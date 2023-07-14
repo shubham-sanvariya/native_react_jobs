460a433b0dc70b0927a34d859cea089270b93fa0
shubham-sanvariya <shubhamsanvariyait.com>
2023-07-14 18:32:49 +0530

    adding layout & index component

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native-web";
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+
+export default Home;
