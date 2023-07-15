d2410b198d432414847337201802e1500cc5a0a6
shubham-sanvariya <shubhamsanvariyait.com>
2023-07-15 13:35:34 +0530

    adding components in index page

--- app/index.js
+++ app/index.js
@@ -1,10 +1,44 @@
-import { View, Text } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { View, ScrollView, SafeAreaView } from "react-native";
+import { COLORS, icons, images, SIZES } from '../constants';
 
 const Home = () => {
+
+    const router = useRouter();
+
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS
+        .lightWhite }}>
+            <Stack.Screen options={{
+                headerStyle: { backgroundColor: COLORS.lightWhite },
+                headerShadowVisible: false,
+                headerLeft: () => (
+                    <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+                ),
+                headerRight: () => (
+                    <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+                ),
+                header: ""
+            }}
+            />
+
+            <ScrollView showsVerticalScrollIndicator
+            ={false}>
+                <View
+                    style= {{
+                        flex: 1,
+                        padding: SIZES.medium
+                    }}
+                >
+                    <Welcome
+
+                    />
+
+                    <Popularjobs />
+                    <Nearbyjobs />
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
